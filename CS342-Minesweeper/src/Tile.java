ebba103d13dd9f166fafd67136380c67f75af81b
Krassimir <Krassimir@10.107.208.161>
2014-02-18 11:14:03 -0600

    edited tile

---
+++ CS342-Minesweeper/src/Tile.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Tile extends JButton{
+
+	boolean bomb; 
+	char currentState; // ? U X
+	
+	public static void main(String args[]) {
+		Tile thisTile = new Tile();
+	}
+	
+	public Tile() {
+		bomb = false;
+		currentState = 'U';
+		
+	}
+	
+	public void setBomb() {
+		this.bomb = true;
+	}
+	
+	public boolean checkForBomb() {
+		if(this.bomb == true)
+			return true; //send to explode
+		else 
+			return false;
+	}
+	
+	
+}
