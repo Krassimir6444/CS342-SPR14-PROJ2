666981244824baaff8b2ceefb6f426f58ef6efcd
Krassimir <Krassimir@10.8.227.80>
2014-02-20 10:36:59 -0600

    Worked on Tile.java (old)

--- CS342-Minesweeper/src/resources/Tile.java
+++ CS342-Minesweeper/src/resources/Tile.java
@@ -1,5 +1,32 @@
 package resources;
+import java.awt.*;
+import javax.swing.*;
 
-public class Tile {
+public class Tile extends JButton{
 
+	boolean bomb; 
+	char currentState; // ? U X
+	
+	public static void main(String args[]) {
+		Tile thisTile = new Tile();
+	}
+	
+	public Tile() {
+		bomb = false;
+		currentState = 'U';
+		
+	}
+	
+	public void setBomb() {
+		this.bomb = true;
+	}
+	
+	public boolean checkForBomb() {
+		if(this.bomb == true)
+			return true; //send to explode
+		else 
+			return false;
+	}
+	
+	
 }
